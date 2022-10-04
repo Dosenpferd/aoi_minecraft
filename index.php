9228341138fdf4c6d4c32c6d4fe2a2c140dfe820
Christian Fischer <christian.fischer@avenit.de>
2022-10-04 18:41:27 +0200

    added index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <title>Age of Ilikia - Mome ist Gome aber nur wenn er HD ist</title>
+    <style>
+      .dark-mode {
+        background-color: #111;
+      }
+
+      .dark-mode * {
+        color: #FFF;
+      }
+
+      body {
+        margin: 0;
+        box-sizing: border-box;
+        font-family: "Comic Sans MS", sans-serif;
+      }
+
+      .page__main {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+      }
+    </style>
+  </head>
+  <body class="dark-mode">
+    <div class="page__main">
+      <h1>This is a private server, pls leave. Yes</h1>
+    </div>
+  </body>
+</html>
