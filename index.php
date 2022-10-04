699da4f13e880ea23cdb6057676054148352c78e
Christian Fischer <christian.fischer@avenit.de>
2022-10-04 18:53:41 +0200

    yes

--- index.php
+++ index.php
@@ -1,29 +1,7 @@
 <html>
   <head>
     <title>Age of Ilikia - Mome ist Gome aber nur wenn er HD ist</title>
-    <style>
-      .dark-mode {
-        background-color: #111;
-      }
-
-      .dark-mode * {
-        color: #FFF;
-      }
-
-      body {
-        margin: 0;
-        box-sizing: border-box;
-        font-family: "Comic Sans MS", sans-serif;
-      }
-
-      .page__main {
-        display: flex;
-        width: 100%;
-        height: 100%;
-        justify-content: center;
-        align-items: center;
-      }
-    </style>
+    <link rel="stylesheet" href="style.css">
   </head>
   <body class="dark-mode">
     <div class="page__main">
